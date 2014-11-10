8769c905b4c650615434d41359d549a5ebed4ac4
Anisuzzaman Khan <aniskhan001@yahoo.com>
2014-11-10 15:41:57 +0600

    basic files added
    header, footer and index added

---
+++ footer.php
@@ -0,0 +1,34 @@
+
+	<!-- Footer -->
+	<footer>
+	<div class="rotate-disk"></div>
+	<div class="footer">
+		<div class="footer-desc">
+			<p>
+				Our <strong>suggestion &amp; recommendation</strong> system will help you to find <strong>great game list</strong> to play next!<br>
+				We are always improving. Please tell your friends about us. The more we engage the greater the result will be!<br>
+				Please check <a href="faq.php">FAQs</a> if you are not sure how to use this site. Visit our site and enjoy your new favorites!
+			</p>
+		</div>
+
+		<div class="social">
+			<h3>We are social!</h3>
+			<p>Be sure to connect with us in the social media and share your thoughts.</p>
+			<ul>
+				<li><a target="_blank" href="https://www.facebook.com/pages/Games-Suggestions/1384639348485961"><img src="imgs/social-facebook.png" alt="Games Suggestions Facebook"></a></li>
+				<li><a target="_blank" href="https://twitter.com/GamesSuggestion"><img src="imgs/social-twitter.png" alt="Games Suggestions Twitter"></a></li>
+				<li><a target="_blank" href="https://plus.google.com/108388021995626188225"><img src="imgs/social-gplus.png" alt="Games Suggestions Google+"></a></li>
+			</ul>
+		</div>
+
+		<p class="copyright">
+			<a target="_blank" href="http://validator.w3.org/check?uri=http%3A%2F%2Fwww.gamessuggestions.com%2F"><img src="imgs/html5.png" alt="HTML5 Validated"></a>
+			<br><em>&copy; 2014 <strong>Games Suggestions</strong></em>
+		</p>
+	</div>
+	</footer>
+	<!-- Footer -->
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title><?php wp_title(); ?></title>
+
+	<meta charset="utf-8">
+	<meta name="robots" content="index,follow">
+	<meta name="keywords" content="game, games, suggestion, recommendation, search, engine">
+	<meta name="description" content="Game suggestions and recommendations for PC, PS3, PS4, Xbox, Nintendo Wii, PS Vita, iPhone/iPad and all available platforms.">
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="autocomplete/jquery.autocomplete.css" />
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+</head>
+
+<body>
+	<!-- Header -->
+	<header>
+		<div class="header">
+			<!-- logo -->
+			<div class="logo">
+				<h1>Games Suggestions</h1>
+				<h2>Game suggestion &amp; recommendation search engine</h2>
+
+				<a href="index.php">
+					<img src="imgs/logo.jpg" alt="Games Suggestions">
+				</a>
+			</div>
+			<!-- logo -->
+
+			<!-- Menu -->
+			<nav class="menu">
+				<ul>
+					<li class="home-btn"><a href="index.php">Home</a><span class="menu-more">GS Home Page</span></li>
+					<li><a href="faq.php">FAQ</a><span class="menu-more">Frequently Asked Question</span></li>
+					<li><a href="about.php">About Us</a><span class="menu-more">About Our Site</span></li>
+					<li><a href="contact.php">Contact</a><span class="menu-more">Contact Us</span></li>
+				</ul>
+			</nav>
+			<!-- Menu -->
+		</div>
+	</header>
