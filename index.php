9970ac9aaec65a55a0da772eddf73606116940e8
Anisuzzaman Khan <aniskhan001@yahoo.com>
2014-11-10 22:27:22 +0600

    Initial post design defined

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php get_header(); ?>
 
-<div class="container">
-	
-</div>
+<div id="content" class="site-content" role="main">
+	<?php /* Start the Loop */ ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<div class="post">
+			<h2> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h2>
+        	<small><?php the_time('F jS, Y') ?></small>
+			<p><?php the_content("read more..."); ?></p>
+		</div>
+	<?php endwhile; ?>
+</div><!-- #content .site-content -->
 
 <?php get_footer(); ?>
