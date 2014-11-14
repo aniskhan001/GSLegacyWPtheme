40fc7f1b891563f6bb6418ed57f3a3358eb6deba
Anisuzzaman Khan <aniskhan001@yahoo.com>
2014-11-14 14:20:25 +0600

    Added ad section inside template

--- index.php
+++ index.php
@@ -2,14 +2,20 @@
 <?php get_header(); ?>
 
 <div id="content" class="site-content" role="main">
-	<?php /* Start the Loop */ ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-		<div class="post">
-			<h2> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h2>
-        	<small><?php the_time('F jS, Y') ?></small>
-			<p><?php the_content("read more..."); ?></p>
-		</div>
-	<?php endwhile; ?>
+	<div id="writing">
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<div class="post">
+				<h2> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h2>
+	        	<small><?php the_time('F jS, Y') ?></small>
+				<p><?php the_content("read more..."); ?></p>
+			</div>
+		<?php endwhile; ?>
+	</div>
+
+	<div id="ads">
+		
+	</div>
 </div><!-- #content .site-content -->
 
 <?php get_footer(); ?>
