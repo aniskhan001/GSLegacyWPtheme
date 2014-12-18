dedd58622a32ab83f6984c329fe92d03af24fa42
Anisuzzaman Khan <aniskhan001@yahoo.com>
2014-12-18 23:35:57 +0600

    Pagination added and Date format changed
    Pagination used by "WP-page Navi" plugin

--- index.php
+++ index.php
@@ -11,10 +11,11 @@
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div class="post">
 				<h3> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h3>
-	        	<small><?php the_time('F jS, Y') ?></small>
+	        	<small><?php the_time('n/j/y') ?></small>
 				<p><?php the_content("read more..."); ?></p>
 			</div>
 		<?php endwhile; ?>
+		<?php wp_pagenavi(); ?> <!-- Pagination by "WP-page Navi" plugin -->
 	</div>
 </div><!-- #content .site-content -->
 
