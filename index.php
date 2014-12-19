689241a64f0b0c2bdc22cb9e81cb247c8b010388
Anisuzzaman Khan <aniskhan001@yahoo.com>
2014-12-19 23:43:53 +0600

    Post date format changed

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div class="post">
 				<h3> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h3>
-	        	<small><?php the_time('n/j/y') ?></small>
+	        	<small><?php the_time('n/j/y g:ia') ?></small>
 				<p><?php the_content("read more..."); ?></p>
 			</div>
 		<?php endwhile; ?>
--- single.php
+++ single.php
@@ -11,7 +11,7 @@
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
             <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-            <small><?php the_time('n/j/y') ?> </small>
+            <small><?php the_time('n/j/y g:ia') ?> </small>
             <div class="entry">
                 <?php the_content(); ?>
             </div>
