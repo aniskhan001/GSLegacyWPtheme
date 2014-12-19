c0d1e88d22ac9793457d1bedbaf9033fb5311e1c
Anisuzzaman Khan <aniskhan001@yahoo.com>
2014-12-20 01:17:15 +0600

    Implemented post view counts

--- index.php
+++ index.php
@@ -10,9 +10,10 @@
 		<?php /* Start the Loop */ ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div class="post">
-				<h3> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h3>
-	        	<small><?php the_time('n/j/y g:ia') ?></small>
-				<p><?php the_content("read more..."); ?></p>
+				<h3> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h3> <!-- post title -->
+	        	<small class="post-time"><?php the_time('n/j/y \a\t g:ia') ?></small> <!-- post date and time -->
+            	<?php echo do_shortcode( '[post-views]' ); ?> <!-- post views -->
+				<div class="entry-home"><?php the_content("read more..."); ?></div>
 			</div>
 		<?php endwhile; ?>
 		<?php wp_pagenavi(); ?> <!-- Pagination by "WP-page Navi" plugin -->
--- single.php
+++ single.php
@@ -10,14 +10,12 @@
     <div id="writing">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-            <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-            <small><?php the_time('n/j/y g:ia') ?> </small>
+            <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2> <!-- post title -->
+            <small class="post-time"><?php the_time('n/j/y  \a\t  g:ia') ?> </small> <!-- post date and time -->
+            <?php echo do_shortcode( '[post-views]' ); ?> <!-- post views -->
             <div class="entry">
                 <?php the_content(); ?>
             </div>
-            <!-- <p class="postmetadata">
-            <?php// the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php //the_category(', ') ?> | <?php //edit_post_link('Edit', '', ' | '); ?>  <?php //comments_popup_link('No Comments »', '1 Comment »', '% Comments »'); ?>
-            </p> -->
         </div>
         <?php endwhile; endif; ?>
 
